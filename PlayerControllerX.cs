68f09e235d12dc69253d87eb3adf8a7344144e67
Ömer Davarcı <78080144+odavarci@users.noreply.github.com>
2022-04-26 10:17:45 +0300

    Add files via upload

---
+++ PlayerControllerX.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private Stopwatch time;
+    private int spawnTime = 500;
+
+    void Start()
+    {
+        time = System.Diagnostics.Stopwatch.StartNew();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            time.Stop();
+            if(time.ElapsedMilliseconds > spawnTime)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                time = System.Diagnostics.Stopwatch.StartNew();
+            }
+            else
+            {
+                time.Start();
+            }
+        }
+    }
+}
